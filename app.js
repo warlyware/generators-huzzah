433654da888596599d7bb7816284efdeb6414e3d
Dan Ward <daniel.ward@forrent.com>
2017-04-04 15:19:49 -0400

    Simple genie call response with generator.

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-function* generator() {
-    yield 'f';
-    console.log('o');
-    yield 'o';
-    console.log('b');
-    yield 'a';
-    console.log('r');
+let answers = [
+  `It is certain`, `It is decidedly so`, `Without a doubt`,
+  `Yes definitely`, `You may rely on it`, `As I see it, yes`,
+  `Most likely`, `Outlook good`, `Yes`, `Signs point to yes`,
+  `Reply hazy try again`, `Ask again later`, `Better not tell you now`,
+  `Cannot predict now`, `Concentrate and ask again`,
+  `Don't count on it`, `My reply is no`, `My sources say no`,
+  `Outlook not so good`, `Very doubtful`
+];
+
+function answer() {
+    return answers[Math.floor(Math.random() * answers.length)];
 }
 
-var gen = generator();
-while (true) {
-    let item = gen.next();
-    if (item.done) {
-        break;
+function* chat() {
+    while (true) {
+        let question = yield '[Genie] ' + answer();
+        console.log(question);
     }
-    console.log(item.value);
 }
+
+var gen = chat();
+gen.next();
+console.log(gen.next('[Me] Will I ever something something?').value);
+console.log(gen.next('[Me] Are you even listening?').value);
